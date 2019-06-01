4f1173e81e376d756464528ec3759b6864cf006f
--unset-all <--unset-all>
2019-06-01 16:24:05 +0900

    add some explanation of FindMax, add return value of func & print

--- FindMax.py
+++ FindMax.py
@@ -1,14 +1,17 @@
-# NguyenU
+# Find the largest value in the array.
 
 def find_max(nums):
-    max = nums[0]
+    max = nums[0] # Comparison from the first element to the last element. 
     for x in nums:
-      if x > max:
+      if x > max:  # if max is smaller than nums[x], then change max to num[x]
         max = x
-    print(max)
+
+    return(max)
 
 def main():
-  find_max([2, 4, 9, 7, 19, 94, 5])
+  a = find_max([2, 4, 9, 7, 19, 94, 5])  
+ # max = 94
+  print(a)
 
 if __name__ == '__main__':
 main()
