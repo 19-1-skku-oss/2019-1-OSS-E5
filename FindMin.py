5292f66b5f563a42f4efbca9973ccd3e437abf0a
--unset-all <--unset-all>
2019-06-02 17:12:31 +0900

    add some explanation of FindMin, change the return value, add print in main func

--- FindMin.py
+++ FindMin.py
@@ -1,12 +1,16 @@
-def main():
-    def findMin(x):
-        minNum = x[0]
-        for i in x:
-            if minNum > i:
-                minNum = i
-        return minNum
+# Find the smallest value in the array.
 
+def findMin(x):
+    minNum = x[0] # Comparison from the first element to the last element.
+
+    for i in x:
+        if minNum > i:  # if min is larger than nums[x], then change min to num[x]
+		minNum = i
+
+	return minNum
+
+def main():
     print(findMin([0,1,2,3,4,5,-3,24,-56])) # = -56
 
 if __name__ == '__main__':
-main()
+    main()
