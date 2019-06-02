2ed70a174fa564df1be28493619824b5e309a530
--unset-all <--unset-all>
2019-06-02 17:02:27 +0900

    push FindMax.py

--- FindMax.py
+++ FindMax.py
@@ -1,17 +1,14 @@
-# Find the largest value in the array.
+# NguyenU
 
 def find_max(nums):
-    max = nums[0] # Comparison from the first element to the last element. 
+    max = nums[0]
     for x in nums:
-      if x > max:  # if max is smaller than nums[x], then change max to num[x]
+      if x > max:
         max = x
-
-    return(max)
+    print(max)
 
 def main():
-  a = find_max([2, 4, 9, 7, 19, 94, 5])  
- # max = 94
-  print(a)
+  find_max([2, 4, 9, 7, 19, 94, 5])
 
 if __name__ == '__main__':
 main()
