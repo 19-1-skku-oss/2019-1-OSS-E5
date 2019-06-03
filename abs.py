b279a1c0aa64d58e70c0fe903bfc25e24af33149
--unset-all <--unset-all>
2019-06-03 15:55:26 +0900

    repush abs.py

--- abs.py
+++ abs.py
@@ -1,24 +1,18 @@
-## algorithms that finds the largest the elements in the array
-
-from abs import absVal
-
 def absVal(num):
-    ## Ex
-    ##>>>absVal([0, 5, 1, 11])
-    ## 11
-    ##>>>absVal([3, -10, -2])
-    ## 3
-
-    temp = absVal(num[0])
-    for i in num:
-	if absVal(i) > temp: ## if there is elements that is larger than temp,
-		temp = i     ## temp is not the largest -> swap
-    return temp              ## return the largest elements. 
+    """
+    Function to fins absolute value of numbers.
+    >>absVal(-5)
+    5
+    >>absVal(0)
+    0
+    """
+    if num < 0:
+        return -num
+    else:
+        return num
 
 def main():
-    a = [1, 2, 3]
-    b = absVal(a)
-    print(b)   ##3
+    print(absVal(-34)) # = 34
 
 if __name__ == '__main__':
-	main()
+main()
