06395076d45d8f5742609440f9c06002b6c20a5e
nicholas369 <nicholas369@naver.com>
2019-06-02 22:02:53 -0700

    Change range and index of priority_queue

--- Python/data_structures/queue/priority_queue.py
+++ Python/data_structures/queue/priority_queue.py
@@ -5,14 +5,15 @@ class PriorityQueue():
 		self.front = 0
 
 	def __str__(self):
-		printed = '<' + str(self.stack)[1:-1] + '>'
+		printed = '<' + str(self.queue)[self.front:self.front+self.length-1] + '>'
 		return printed
 
 	def put(self, item):
-		for i in range(self.length):
+		for i in range(self.front, self.front+self.length):
 			if(item > self.queue[i]):
 				self.insert(i, item)
 		self.length = self.length + 1
+		return 1
 
 	def get(self):
 		self.length = self.length - 1
@@ -22,7 +23,7 @@ class PriorityQueue():
 		return dequeued
 
 	def front(self):
-		return self.queue[0]
+		return self.queue[self.front]
 	
 	def size(self):
 		return self.length
