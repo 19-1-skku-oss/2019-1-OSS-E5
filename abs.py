ba041036218f8e37f25e897689e92ce0eab15623
--unset-all <--unset-all>
2019-06-03 15:24:52 +0900

    finds the largest elements in the array, add some explanations

--- abs.py
+++ abs.py
@@ -1,18 +1,24 @@
+## algorithms that finds the largest the elements in the array
+
+from abs import absVal
+
 def absVal(num):
-    """
-    Function to fins absolute value of numbers.
-    >>absVal(-5)
-    5
-    >>absVal(0)
-    0
-    """
-    if num < 0:
-        return -num
-    else:
-        return num
+    ## Ex
+    ##>>>absVal([0, 5, 1, 11])
+    ## 11
+    ##>>>absVal([3, -10, -2])
+    ## 3
+
+    temp = absVal(num[0])
+    for i in num:
+	if absVal(i) > temp: ## if there is elements that is larger than temp,
+		temp = i     ## temp is not the largest -> swap
+    return temp              ## return the largest elements. 
 
 def main():
-    print(absVal(-34)) # = 34
+    a = [1, 2, 3]
+    b = absVal(a)
+    print(b)   ##3
 
 if __name__ == '__main__':
-main()
+	main()
