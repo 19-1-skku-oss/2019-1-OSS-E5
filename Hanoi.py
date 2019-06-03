0bb64e0ca12b8be653d4061cda56119712c48919
--unset-all <--unset-all>
2019-06-03 15:35:55 +0900

    change the numbers of the pararemet of Tower_OF_Hanoi(to reduce unnecessary parts)

--- Hanoi.py
+++ Hanoi.py
@@ -7,12 +7,13 @@ log = logging.getLogger()
 logging.basicConfig(level=logging.DEBUG)
 
 
-def Tower_Of_Hanoi(n, source, dest, by, movement):
+def Tower_Of_Hanoi(n, source, aux, destination):   
     if n >= 1:
-	Tower_Of_Hanoi(n-1, source, movement, by)
+	Tower_Of_Hanoi(n-1, source, destination, aux)
 	moveDisk(source, by)
-	Tower_Of_Hanoi(n-1, movement, by, source)
-## recursive fun
+	Tower_Of_Hanoi(n-1, destination, aux, source)
+
+## recursive func
 
 def moveDisk(fp, tp):   ## the function that print the moving situation of the tower.
 	print(('Moving disk from', fp, 'to', tp))
@@ -21,8 +22,10 @@ def moveDisk(fp, tp):   ## the function that print the moving situation of the t
 def main():
 	height = int(input('Height of hanoi: '))
 	## user input the number of height of hanoi tower.
+        ## the number of total disks 
 
-	Tower_Of_Hanoi(height, 'A', 'B', 'C')
+	Tower_Of_Hanoi(height, 'A', 'B', 'C')   
+        ## there are 3 tower named A, B, C, and disks (the number of height)
 
 
 if __name__ == '__main__':
