7479e44a9baa35284a68d733dd9f95a5a01999c5
pitlover <pitlover@naver.com>
2019-06-05 18:31:26 +0900

    add explanation of simson_rule

---
+++ simpson_rule.py
@@ -0,0 +1,57 @@
+'''
+Numerical integration or quadrature for a smooth function f with known values at x_i
+This method is the classical approch of suming 'Equally Spaced Abscissas' 
+method 2: 
+"Simpson Rule"
+'''
+
+
+'''
+What's is the simson rules?
+방정식 P(x) 라는 이차방정식이 있으면, f(x)의 근사값을 구한다.
+여기서 P(x)는 P(x) 위에 있는 임의의 a, b, 그리고 그 둘의 중간값 m = (a+b)/2 을 구하고
+중간값의 f(x)와 같은 값을 갖는 근사식이다. 
+
+P(x) 식을 전개하면서 다음 심프신 공식을 구한다. 
+https://ko.wijipedia.org/wiki/심프신_공식 참고
+
+'''
+from __future__ import print_function
+
+
+def method_2(boundary, steps):
+# "Simpson Rule"
+# int(f) = delta_x/2 * (b-a)/3*(f1 + 4f2 + 2f_3 + ... + fn)
+	h = (boundary[1] - boundary[0]) / steps
+	a = boundary[0]
+	b = boundary[1]
+	x_i = makePoints(a,b,h)
+	y = 0.0
+	y += (h/3.0)*f(a)
+	cnt = 2
+	for i in x_i:
+		y += (h/3)*(4-2*(cnt%2))*f(i)	
+		cnt += 1
+	y += (h/3.0)*f(b)
+	return y
+
+def makePoints(a,b,h):
+	x = a + h
+	while x < (b-h):
+		yield x
+		x = x + h
+
+def f(x): #enter your function here
+	y = (x-0)*(x-0)
+	return y
+
+def main():
+	a = 0.0 #Lower bound of integration
+	b = 1.0	#Upper bound of integration
+	steps = 10.0		#define number of steps or resolution
+	boundary = [a, b]	#define boundary of integration
+	y = method_2(boundary, steps)
+	print('y = {0}'.format(y))
+
+if __name__ == '__main__':
+main()
