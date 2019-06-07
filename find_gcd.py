173ad253dd367611964794e2172f1ea222a51291
pitlover <pitlover@naver.com>
2019-06-07 20:36:02 +0900

    advanced version of finding gcd with 유클리드 호제법

--- find_gcd.py
+++ find_gcd.py
@@ -1,5 +1,7 @@
 ## algorithms that find greatest common multiple
 ## 최대공약수를 구하는 함수
+## 유클리드 호제법 이용!
+
 '''
 a mod b = 0 이라면, gcd(a,b) = b
 a mod b !=0 이라면, gcd(a,b) = gcd(b, a mod b) 성립!!
@@ -7,12 +9,13 @@ a mod b !=0 이라면, gcd(a,b) = gcd(b, a mod b) 성립!!
 '''
 
 def find_gcd(num_1, num_2):
-    if num_1 < num_2:
-	(num_1, num_2) = (num_2, num_1)  ## swap num_1 & num_2
-    while num_2 != 0:
-	(num_1, num_2) = (num_2, num_1 % num_2)
-
-    return num_1
+   mod = a%b
+   while mod>0:
+	num_1 = num_2
+	num_2 = mod
+	mod = num_1 % num_2
+   
+   return num_2
 
 def main():
     num_1 = input("Input num_1")
