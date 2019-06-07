9412d18bfcbc63224823ff56d0291732930bee25
jaewan7599 <jaewan7599@skku.edu>
2019-06-07 10:58:14 +0900

    Make traverse function

--- BTree.py
+++ BTree.py
@@ -1,11 +1,24 @@
-class BTreeNode:
+class BTreeNode(object):
 	def __init__(self, degree, leaf):
 		self.leaf = leaf
 		self.keys = []
 		self.childs = []
 		self.degree = degree
 
-class BTree:
+class BTree(object):
 	def __init__(self, degree):
 		self.degree = degree - 1
 		self.root = BTreeNode(degree, True)
+
+	def traverse(cur_node):
+		if len(self.root.keys) != 0:
+			for i in range(len(cur_node.keys)):
+				if not cur_node.leaf:
+					traverse(cur_node.childs[i])
+				print(" ");
+				print(cur_node.keys[i])
+
+		if not cur_node.leaf:
+			traverse(cur_node.childs[len(cur_node.keys)]
+
+	
