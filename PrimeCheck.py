9429b8813b0131fac04f2291c9ced74abc128012
pitlover <pitlover@naver.com>
2019-06-07 20:09:56 +0900

    enhanced PrimeCheck - 제곱근 이용법

--- PrimeCheck.py
+++ PrimeCheck.py
@@ -7,42 +7,32 @@ import unittest
 
 
 def primeCheck(number):
-    """수
-    A number is prime if it has exactly two dividers: 1 and itself.
+    """
+    A number is prime if it has exactly two dividers: 1 and itself.수
 
-    소수의 정의 : 1과 자기 자신을 약수로 가지는 
+    소수의 정의 : 1과 자기 자신을 약수로 가지는 수
+	만약 N = a* b 라면, N은 a, b 모두 다 나누어진다.
+	-> N의 제곱근 r을 기준으로 곱해서 N이 될 수 있는 약수들이 같은 개수로 분포한다.
+	=> 따라서 N이 소수인지를 알고 싶으면 N의 제곱근까지만 검사하면 된다!
     """
     if number < 2:
         # Negatives, 0 and 1 are not primes
         return False
-    if number < 4:
-        # 2 and 3 are primes
-        return True
-    if number % 2 == 0:
-        # Even values are not primes
-        return False
+   
+   if n == 2:
+	return True
 
-    # Except 2, all primes are odd. If any odd value divide
-    # the number, then that number is not prime.
-    # 3이하의 짝수인 소수는 2일 뿐 -> 그 외 모든 짝수는 소수가 아니다!
+   if n % 2 == 0:
+	return False
+    
+   l = math.sqrt(n)
 
-    odd_numbers = range(3, int(math.sqrt(number)) + 1, 2)
-    return not any(number % i == 0 for i in odd_numbers)
+   for i in range(3,l,2):
+	if n%i == 0:
+		return False
 
 
-class Test(unittest.TestCase):
-    def test_not_primes(self):
-        self.assertFalse(primeCheck(-19),
-                "Negative numbers are not prime.")
-        self.assertFalse(primeCheck(0),
-                "Zero doesn't have any divider, primes must have two")
-        self.assertFalse(primeCheck(1),
-                "One just have 1 divider, primes must have two.")
-        self.assertFalse(primeCheck(2 * 2))
-        self.assertFalse(primeCheck(2 * 3))
-        self.assertFalse(primeCheck(3 * 3))
-        self.assertFalse(primeCheck(3 * 5))
-        self.assertFalse(primeCheck(3 * 5 * 7))
+    return True
 
 
 if __name__ == '__main__':
