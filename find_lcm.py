ffec7e75f727eaee9a2fdc38f64e96cac7a6bbee
pitlover <pitlover@naver.com>
2019-06-07 20:23:53 +0900

    fix if-else, add some explanations

--- find_lcm.py
+++ find_lcm.py
@@ -1,6 +1,13 @@
+##algorithms that find least common multiple
+## 최소공배수를 구하는 함수
+
 def find_lcm(num_1, num_2):
-    max = num_1 if num_1 > num_2 else num_2
+    if(num_1 > num_2)
+	max = num_1
+    else:
+	max = num_2
     lcm = max
+   
     while (True):
         if ((lcm % num_1 == 0) and (lcm % num_2 == 0)):
             break
@@ -9,10 +16,10 @@ def find_lcm(num_1, num_2):
 
 
 def main():
-    num_1 = 12
-    num_2 = 76
+    num_1 = input("Input num_1")
+    num_2 = input("Input num_2")
     print(find_lcm(num_1, num_2))
 
 
 if __name__ == '__main__':
-main()
+	main()
