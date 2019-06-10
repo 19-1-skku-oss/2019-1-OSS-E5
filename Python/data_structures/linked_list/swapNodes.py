97d959d410be0dcc463566b7c3883e8a503c13d4
jun-honey <psj97047@naver.com>
2019-06-11 03:22:07 +0900

    Add swap node final

--- Python/data_structures/linked_list/swapNodes.py
+++ Python/data_structures/linked_list/swapNodes.py
@@ -1,3 +1,6 @@
+'''
+In this py code you can learn swaping node.
+'''
 class Node:
     def __init__(self, data):
         self.data = data;
@@ -5,71 +8,73 @@ class Node:
 
 
 class Linkedlist:
-    def __init__(self):
-        self.head = None
+    def __init__(self): # Track only by head
+        self.head = None 
 
-    def print_list(self):
+    def print_list(self): # Display linked list
+        if self.head == None:
+            return print("None")
         temp = self.head
+        print(temp.data,end=" ")
+        temp = temp.next
         while temp is not None:
-            print(temp.data)
+            print("-->",temp.data,end=" ")
             temp = temp.next
-
-# adding nodes
+        print()
+# Adding nodes
     def push(self, new_data):
         new_node = Node(new_data)
         new_node.next = self.head
         self.head = new_node
 
-# swapping nodes
+# Swapping nodes (Look carefully to execption : head node)
     def swapNodes(self, d1, d2):
         prevD1 = None
         prevD2 = None
         if d1 == d2:
             return
         else:
-            # find d1
+            # Find d1
             D1 = self.head
             while D1 is not None and D1.data != d1:
                 prevD1 = D1
                 D1 = D1.next
-                # find d2
+            # Find d2
             D2 = self.head
             while D2 is not None and D2.data != d2:
                 prevD2 = D2
                 D2 = D2.next
             if D1 is None and D2 is None:
                 return
-            # if D1 is head
+            # If D1 is head
             if prevD1 is not None:
                 prevD1.next = D2
             else:
                 self.head = D2
-            # if D2 is head
+            # If D2 is head
             if prevD2 is not None:
                 prevD2.next = D1
             else:
                 self.head = D1
+                
             temp = D1.next
             D1.next = D2.next
             D2.next = temp
 
-# swapping code ends here
+# Swapping code ends here
 
 
 
-if __name__ == '__main__':
+if __name__ == '__main__': # Main for test
     list = Linkedlist()
     list.push(5)
     list.push(4)
     list.push(3)
     list.push(2)
     list.push(1)
-
+    
     list.print_list()
 
     list.swapNodes(1, 4)
     print("After swapping")
     list.print_list()
-
-
-
